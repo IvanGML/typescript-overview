9c6745a1749ad0814f0c8836138e71198419b10e
IvanGML <somovlife@gmail.com>
2019-12-08 23:10:33 +0300

    TS-OVER: add class with buble sorting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello! :)');
+class Sorter {
+    constructor(public collection: number[]) {};
+
+    sort():void {
+        let arrLength = this.collection.length;
+        while(arrLength > 0) {
+            for(let i = 0; i < arrLength-1; i++) {
+                if (this.collection[i] > this.collection[i+1]) {
+                    let temp = this.collection[i];
+                    this.collection[i] = this.collection[i+1];
+                    this.collection[i+1] = temp;
+                }
+            }
+            arrLength--;
+        }
+    }
+}
+
+const sorter = new Sorter([3, -3, 2, 10, -10]);
+sorter.sort();
