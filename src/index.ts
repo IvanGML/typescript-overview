ab5b41ee920ac1130a2a812cee06d5ffe49e439d
Ivan Somau <Ivan_Somau@epam.com>
2019-12-18 20:21:28 +0300

    TS-OVER: add basic report to stats-project

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import fs from 'fs';
 
 const file = fs.readFileSync('./football.csv', {
     encoding: 'utf-8',
+})
+.split('\n')
+.map((item: string): string[] => item.split(','));
+
+let manUnitedWins = 0;
+
+file.forEach((item:string[], index:number)=>{
+    let teamWinHome = item.indexOf('Man United') === 1 && item.indexOf('H') === 5;
+    let teamWinAway = item.indexOf('Man United') === 2 && item.indexOf('A') === 5
+    if(teamWinHome || teamWinAway) manUnitedWins++;
 });
 
-console.log(file);
+console.log(`Man United wins: ${manUnitedWins} times.`);
