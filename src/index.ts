c5bc7ffd95df09b9c0d9f09d93150746c246b8f7
IvanGML <somovlife@gmail.com>
2019-12-19 19:14:46 +0300

    TS-OVER: create enum for strings constants

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,17 @@ const file = fs.readFileSync('./football.csv', {
 .split('\n')
 .map((item: string): string[] => item.split(','));
 
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+}
+
 let manUnitedWins = 0;
 
 file.forEach((item:string[], index:number)=>{
-    let teamWinHome = item.indexOf('Man United') === 1 && item.indexOf('H') === 5;
-    let teamWinAway = item.indexOf('Man United') === 2 && item.indexOf('A') === 5
+    let teamWinHome = item.indexOf('Man United') === 1 && item.indexOf(MatchResult.HomeWin) === 5;
+    let teamWinAway = item.indexOf('Man United') === 2 && item.indexOf(MatchResult.AwayWin) === 5;
     if(teamWinHome || teamWinAway) manUnitedWins++;
 });
 
