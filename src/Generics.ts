409ca8371cd340bdeb896c7338a220cae2efbb1d
IvanGML <somovlife@gmail.com>
2019-12-27 15:30:19 +0300

    TS-OVER: add one more example of generics constraints

--- src/Generics.ts
+++ src/Generics.ts
@@ -1,3 +1,4 @@
+// example #1
 class ArrayOfAnything<T> {
     constructor(public elements: T[]) {}
     get(elem: number): T {
@@ -7,3 +8,29 @@ class ArrayOfAnything<T> {
 
 const arrayOfStrings = new ArrayOfAnything<string>(['a', 'b', 'c']);
 const arrayOfNumbers = new ArrayOfAnything<number>([1, 2, 3]);
+
+// example #2
+class Car {
+    constructor(public model: string) {}
+    print():void {
+        console.log(`this is the Car. model: ${this.model}`);
+    }
+}
+
+class House {
+    print():void {
+        console.log('this is the Car.');
+    }
+}
+
+interface Printable {
+    print(): void;
+}
+
+function printIt<T extends Printable>(array: T[]):void {
+    for (let index = 0; index < array.length; index++) {
+        array[index].print();
+    }
+}
+
+printIt<Car>([new Car('Shelby'), new Car('Roadster')]);
