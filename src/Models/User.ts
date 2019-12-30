95fb999c033331bb95ae5698c91802e93f20e17f
IvanGML <somovlife@gmail.com>
2019-12-30 15:39:45 +0300

    TS-OVER: add attributes into User initialization

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -13,4 +13,9 @@ const rootUrl: string = 'http://localhost:3000/users';
 export class User {
     public events: Eventing = new Eventing();
     public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+    public attributs: Attributes<UserProps>;
+
+    constructor(attrs: UserProps) {
+        this.attributs = new Attributes<UserProps>(attrs);
+    }
 };
