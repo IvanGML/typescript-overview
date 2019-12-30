142a770f80efe8232c239a7aebd546a0b11fd9f1
IvanGML <somovlife@gmail.com>
2019-12-30 20:44:58 +0300

    TS-OVER: create first view class UserForm

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { User } from './Models/User';
+import { UserForm } from './View/UserForm';
 
-const users = User.userCollection();
-users.fetch();
-users.events.on('change', () => console.log('%c Users have been fetched.', 'color: green'));
-console.log(users.models);
+const userForm = new UserForm(document.getElementById('root'));
+userForm.render();
+
+// const users = User.userCollection();
+// users.fetch();
+// users.events.on('change', () => console.log('%c Users have been fetched.', 'color: green'));
+// console.log(users.models);
 
 // const user = User.buildUser({ });
 // user.set({ id:10, name: 'John', age: 30 });
